d2765d5f049230952f9420a1b66c54be908ce95b
fa <francisco.azevedo@advertbrands.com>
2019-07-28 22:02:41 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# timber
-a simple timber.io php sdk
+This library provides an API to send logging to timber.io services.
+
+Install with Composer
+------------
+Add `abgit/timber` as a dependency and run composer update:
+
+```
+"require": {
+    ...
+    "abgit/timber" : "0.1.*"
+    ...
+}
+```
+
+Create a timber.io source
+------------
+Create a "HTTP API" source and find source credentials "Source ID" and "Api KEY".
+
+
+```php
+<?php
+
+// import dependencies
+require 'vendor/autoload.php';
+
+// get the timber class instance
+$timber = new timber\log( YOUR_SOURCE_ID, YOUR_SOURCE_APIKEY );
+
+// Add messages
+$timber->warning('another message', ['code' => 45 ] )
+       ->error('another message', ['code' => 46 ] )
+       ->info('another message', ['code' => 46 ] )
+       ->debug('another message', ['code' => 46 ] );
+
+// Send messages to timber
+$timber->send(); 
+```
--- src/log.php
+++ src/log.php
@@ -10,41 +10,42 @@ class log{
     private $source_id;
     private $source_apikey;
 
+    const INFO    = 'info';
+    const WARNING = 'warning';
+    const ERROR   = 'error';
+    const DEBUG   = 'debug';
+
     public function __construct( $source_id, $source_apikey ){
         $this->source_id = $source_id;
         $this->source_apikey = $source_apikey;
     }
 
-    public function info( $message, $meta = null, $timestamp = null ){
-        $message = $this->message( 'info', $message, $meta, $timestamp );
-        $this->messages[] = $message;
-        return $message;
+    public function & info( $message, $meta = null, $timestamp = null ){
+        $this->messages[] = $this->message( log::INFO, $message, $meta, $timestamp );
+        return $this;
     }
 
-    public function warning( $message, $meta = null, $timestamp = null ){
-        $message = $this->message( 'warning', $message, $meta, $timestamp );
-        $this->messages[] = $message;
-        return $message;
+    public function & warning( $message, $meta = null, $timestamp = null ){
+        $this->messages[] = $this->message( log::WARNING, $message, $meta, $timestamp );
+        return $this;
     }
 
-    public function error( $message, $meta = null, $timestamp = null ){
-        $message = $this->message( 'error', $message, $meta, $timestamp );
-        $this->messages[] = $message;
-        return $message;
+    public function & error( $message, $meta = null, $timestamp = null ){
+        $this->messages[] = $this->message( log::ERROR, $message, $meta, $timestamp );
+        return $this;
     }
 
-    public function debug( $message, $meta = null, $timestamp = null ){
-        $message = $this->message( 'debug', $message, $meta, $timestamp );
-        $this->messages[] = $message;
-        return $message;
-    }
-
-    public function message( $level, $message, $meta, $timestamp ){
-        return json_encode( array( "level" => $level, "message" => $message ) + ( is_array( $meta ) ? $meta : array() ) + ( is_null( $timestamp ) ? array() : array( 'dt' => date( 'c', $timestamp ) ) ) );
+    public function & debug( $message, $meta = null, $timestamp = null ){
+        $this->messages[] = $this->message( log::DEBUG, $message, $meta, $timestamp );
+        return $this;
     }
 
     public function send(){
         return $this->post( $this->messages );
+    }
+
+    public function message( $level, $message, $meta, $timestamp ){
+        return json_encode( array( "level" => $level, "message" => $message ) + ( is_array( $meta ) ? $meta : array() ) + ( is_null( $timestamp ) ? array() : array( 'dt' => date( 'c', $timestamp ) ) ) );
     }
 
     public function post( $messages ){
